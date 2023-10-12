79c8c0aaecb53568a43281b38e2b11b77492d8ae
YoonJieut <qmdldos@gmail.com>
2023-10-12 22:58:52 +0900

    태그 메이커 모듈 생성, 매개변수는 pokemonKorName과 TagName까지

--- model/makeLiTag.js
+++ model/makeLiTag.js
@@ -1,1 +1,14 @@
 // 포켓몬스터 한글 배열을 html li 태그로 만들어주는 함수
+function setTagMakerPokemonList(pokemonKorName, tagName){
+  if(Array.isArray(pokemonKorName)===true){
+    let arrayLiTagPokemonList  = [];
+    for (let i=0; i <pokemonKorName.length; i++){
+      arrayLiTagPokemonList.push(`<${tagName}>${pokemonKorName[i]}</${tagName}>`)
+    }
+    return arrayLiTagPokemonList.join("");
+  }else{
+    return new Error(" 배열이 아님당 ");
+  }
+}
+
+module.exports = setTagMakerPokemonList;
